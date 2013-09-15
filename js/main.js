f618cc38e8eb5023a3494419fd1d209c7a906ce3
George Lesica <glesica@gmail.com>
2013-09-15 13:49:23 -0600

    Add interface.

--- js/main.js
+++ js/main.js
@@ -8,3 +8,39 @@
     }
     countdown.blocks('#visual', params).run();
 })();
+
+YUI().use('anim', 'button', 'calendar', function(Y) {
+    var params = {};
+
+    var startDate = new Y.Calendar({
+        contentBox: '#start-date',
+        date: new Date(),
+        on: {
+            'selectionChange': function(e) {
+                params.ten = e.newSelection[0].valueOf();
+            }
+        }
+    }).render();
+
+    var endDate = new Y.Calendar({
+        contentBox: '#end-date',
+        date: new Date(),
+        on: {
+            'selectionChange': function(e) {
+                params.zero = e.newSelection[0].valueOf();
+            }
+        }
+    }).render();
+
+    var submitButton = new Y.Button({
+        srcNode: '#submit',
+        on: {
+            'click': function() {
+                params.title = Y.one('#title').get('value');
+                var hash = btoa(JSON.stringify(params));
+                window.location.assign('#' + hash);
+                window.location.reload(true);
+            }
+        }
+    }).render();
+});
